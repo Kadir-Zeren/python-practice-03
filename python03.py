0b500bbf5ae3078b590f640382ef60c9a4e46468
kadir-zeren <k4dzeren@gmail.com>
2024-10-11 16:33:11 +0300

    practice

--- python03.py
+++ python03.py
@@ -175,3 +175,10 @@ print([] or "be happy!")
 print(None or ())
 print({} or 0)
 print({0} or False)
+
+fruit = 'Orange'
+print('Word: ', fruit)
+print('First letter:', fruit[0])
+print('Second letter :', fruit[1])
+print("3rd to 5th letters :", fruit[2:5])
+print("Letter all after 3rd :", fruit[2:])
